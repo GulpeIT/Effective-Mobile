38dc4b3c3db6aa520fac6d00c6c254949c0fc14d
GulpeIT <grishawork-01@mail.ru>
2026-03-24 16:46:44 +0300

    create role model

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -1,9 +1,11 @@
 from .db_helper import db_helper
 from .base import Base
 from .user import User
+from .role import Role
 
 __all__=[
     "db_helper",
     "Base",
     "User",
+    "Role",
 ]
---
+++ src/core/models/role.py
@@ -0,0 +1,18 @@
+from typing import TYPE_CHECKING
+from core.models import Base
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+
+if TYPE_CHECKING:
+    from core.models import User
+
+class Role(Base):
+    __tablename__ = 'roles'
+    
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(unique=True)
+    
+    user: Mapped['User'] = relationship(
+        'role'
+    )
