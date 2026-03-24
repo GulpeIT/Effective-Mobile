4d3305684609fb5e16876a76a41ae4459bf5986f
GulpeIT <grishawork-01@mail.ru>
2026-03-24 16:47:01 +0300

    update user model

--- src/core/models/user.py
+++ src/core/models/user.py
@@ -1,17 +1,25 @@
 from typing import TYPE_CHECKING
-
 from core.models import Base
-
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import mapped_column, relationship
+
+if TYPE_CHECKING:
+    from core.models import Role
 
 class User(Base):
     __tablename__ = "users"
     
     id: Mapped[int] = mapped_column(primary_key=True)
+    id_role: Mapped[int] = mapped_column(ForeignKey('roles.id'))
     name: Mapped[str]
     surname: Mapped[str]
     patronymic: Mapped[str]
     password: Mapped[str] = mapped_column(unique=True)
     email: Mapped[str] = mapped_column(unique=True)
     is_active: Mapped[bool] 
+    
+    role: Mapped['Role'] = relationship(
+        back_populates='user'
+    )
+    
