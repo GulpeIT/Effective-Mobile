17404097249126bde94a6eadf84268f1372f89c3
GulpeIT <grishawork-01@mail.ru>
2026-03-25 20:32:49 +0300

    remake revision

--- src/alembic/versions/2026_03_25_1619-7f2f0b7d724c_init.py
+++ src/alembic/versions/2026_03_25_1619-7f2f0b7d724c_init.py
@@ -1,7 +1,9 @@
 """init
-Revision ID: 5b77a65b05b3
+
+Revision ID: 7f2f0b7d724c
 Revises:
-Create Date: 2026-03-23 13:02:53.882328
+Create Date: 2026-03-25 16:19:09.205143
+
 """
 
 from typing import Sequence, Union
@@ -9,7 +11,7 @@ from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 
-revision: str = "5b77a65b05b3"
+revision: str = "7f2f0b7d724c"
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -17,14 +19,26 @@ depends_on: Union[str, Sequence[str], None] = None
 
 def upgrade() -> None:
     op.create_table(
+        "roles",
+        sa.Column("id", sa.Integer(), autoincrement=True, nullable=False),
+        sa.Column("name", sa.String(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+        sa.UniqueConstraint("name"),
+    )
+    op.create_table(
         "users",
-        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("id", sa.Integer(), autoincrement=True, nullable=False),
+        sa.Column("id_role", sa.Integer(), nullable=False),
         sa.Column("name", sa.String(), nullable=False),
         sa.Column("surname", sa.String(), nullable=False),
         sa.Column("patronymic", sa.String(), nullable=False),
         sa.Column("password", sa.String(), nullable=False),
         sa.Column("email", sa.String(), nullable=False),
         sa.Column("is_active", sa.Boolean(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["id_role"],
+            ["roles.id"],
+        ),
         sa.PrimaryKeyConstraint("id"),
         sa.UniqueConstraint("email"),
         sa.UniqueConstraint("password"),
@@ -33,3 +47,4 @@ def upgrade() -> None:
 
 def downgrade() -> None:
     op.drop_table("users")
+    op.drop_table("roles")
